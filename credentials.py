854ad266bdfaf815b9f63d614cf52ec228d9359e
Sammy Muchiri <dev.miano@gmail.com>
2022-04-25 01:55:13 +0300

    Added the credentials class

--- credentials.py
+++ credentials.py
@@ -1,1 +1,21 @@
 from user import User
+import random
+
+'''class that creates new credentials for a user'''
+class Credentials:
+  
+  
+  '''initialize new credentials array'''
+  credentials = []
+  
+  
+  '''init method that defines properties for a new credentials object'''
+  def __init__(self, site, username, email, password):
+    '''arguments for the credentials object'''
+    
+    self.site = site
+    self.username = username
+    self.email = email
+    self.password = password
+    
+  
