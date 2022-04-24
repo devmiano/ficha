82fb8c6cbdf09fe1db9ee140d56c7eb9fdd4b22b
Sammy Muchiri <dev.miano@gmail.com>
2022-04-25 01:06:14 +0300

    Created the user class, object and method

---
+++ user.py
@@ -0,0 +1,32 @@
+class User:
+  '''a class that generates a new user'''
+  pass
+
+  '''initialize new users array'''
+  users = []
+  
+  '''an init method that defines properties for a new user object'''
+  def __init__(self, fname, lname, username, email, password):
+    '''arguments for the user object'''
+    
+    self.fname = fname
+    self.lname = lname
+    self.username = username
+    self.email = email
+    self.password = password
+    
+  '''save users in the users array'''
+  def save_user(self):
+    User.users.append(self)
+    
+  '''delete users in the users array'''
+  def delete_user(self):
+    User.users.remove(self)
+    
+  
+  '''a method to retrieve a user in the users array for sign in'''
+  @classmethod
+  def get_user(cls, username, password):
+    for user in cls.users:
+      if user.username == username and user.password == password:
+        return True
