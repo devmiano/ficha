237d443514ae4c1641c76c787aef19f0106d8434
Sammy Muchiri <dev.miano@gmail.com>
2022-04-25 04:08:56 +0300

    Added functions to create and save new credentials in the run.py file

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import random
+import random, string
 import maskpass
 from user import User
 from credentials import Credentials
@@ -14,3 +14,24 @@ def new_user(fname, lname, username, email, password):
 def save_user(user):
   '''function that saves a new user from the save_user method'''
   User.save_user(user)
+  
+def new_credentials(site, username, email, password):
+  '''function that creates new credentials from the credentials class'''
+  
+  new_credentials = Credentials(site, username, email, password)
+  return new_credentials
+
+def save_credentials(credentials):
+  '''function that saves new credentials from the save_credentials method'''
+  
+  return Credentials.save_credentials(credentials)
+  
+
+def display_vault():
+  '''function that displays all credentials from the vault method'''
+  
+  return Credentials.vault()
+
+
+
+
