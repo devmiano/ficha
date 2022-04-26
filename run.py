c72ea2972c8a7fb504283728eb8098179b2b791c
Sammy Muchiri <dev.miano@gmail.com>
2022-04-27 00:09:56 +0300

    added pyperclip and filter to the credentials

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 import random, string
 import maskpass
+import pyperclip
 from user import User
 from credentials import Credentials
 
@@ -49,8 +50,8 @@ def createpwd(length, char=string.ascii_uppercase + string.ascii_lowercase + str
 
 
 def main():
-  print("\n\n================== Ficha Password Manager ====================")
-  print('To get started please choose one of the following options from the menu below')
+  print("\n\n================== Ficha Password Manager ====================\n")
+  print('To get started please choose one of the following options from the menu below \n')
   
   while True: 
     print('Main Menu: \n a - Create new Ficha account \n b - Log into your Ficha account \n x - Close Application \n')
@@ -58,71 +59,58 @@ def main():
     short_code = input("Enter option: ").lower()
     
     if short_code == 'a':
-      print('Enter first name:')
-      fname = input().strip()
-      print('Enter last name:')
-      lname = input().strip()
-      print('Enter username:')
-      username = input().strip()
-      print('Enter email:')
-      email = input().strip()
-      
-      print('Create your master password:')
-      password = maskpass.askpass(prompt="Password:", mask='*')
+      fname = input('Enter first name: ').strip()
+      lname = input('Enter last name: ').strip()
+      username = input('Enter username: ').strip()
+      email = input('Enter email: ').strip()
+      print('\n')
+      password = maskpass.askpass(prompt="Create your master Password: ", mask='*')
       
       save_user(create_user(fname, lname, username, email, password))
       
       print('\n')
-
-      print(f'Username: {username} \nEmail: {email} \nPassword: {password}')
-      
-      print('\nPlease now log in to start generating and saving your passwords')
+      print('Please now log in to start generating and saving your passwords')
+      print('\n')
       
     elif short_code == 'b':
-      print('\n Log in \n')
-      print('Enter username:')
-      username = input().strip()
-      print('Enter your master password:')
-      password = maskpass.askpass(prompt="Password:", mask='*')
+      print("\n\n================== Log in ====================\n")
+      username = input('Enter username: ').strip()
+      password = maskpass.askpass(prompt="Enter your master Password: ", mask='*')
       
       username = User.get_user(username, password)
       
       if username == username:
         print('\nLogin successful!')
-        
-        print(f'Welcome {fname} {lname} to Ficha Password Manager')
+        print(f"\n================== {fname.capitalize()} {lname.capitalize()}'s Vault ====================\n")
+        print(f'Welcome to Ficha Password Manager {username}')
         
         while True:
           print('\n')
           
-          print('App Menu: \n c - Create new Credentials \n d - View your Vault \n e - Log out \n')
+          print('App Menu: \n c - Create new Credentials \n d - View your Vault \n e - Copy Credentials \n f - Log out \n ')
           
           short_code = input("Enter option: ").lower()
           
+          print('\n')
           if short_code == 'c':
-            print('\n')
-        
             print('Create new Credentials')
-            print('Enter site name:')
-            site = input().strip()
-            print('Enter site username:')
-            site_username = input().strip()
-            print('Enter site email:')
-            site_email = input().strip()
+            print('\n')
+            site = input('Enter site name: ').strip()
+            site_username = input('Enter site username: ').strip()
+            site_email = input('Enter site email: ').strip()
             
             while True:
               print('\n')
         
-              print('Enter "y" to create new Credentials with your password \nEnter "n" to create new Credentials with a generated password.')
+              print('Enter "m" to create your password manually \nEnter "n" to generate a password.')
               short_code = input("Enter option: ").lower()
               
               if short_code == 'y':
-                print('Enter your site password:')
-                site_password = maskpass.askpass(prompt="Password:", mask='*')
+                site_password = maskpass.askpass(prompt="Enter your site Password: ", mask='*')
                 break
               elif short_code == 'n':
-                print('Enter a password length:')
-                length = input().strip()
+                print()
+                length = input('Enter a password length: ').strip()
                 site_password = createpwd(length)
                 break
               else:
@@ -134,12 +122,14 @@ def main():
             
             print(f'Successfully created new credentials for {username} \n Site name: {site}\n Username: {site_username} \n Email: {site_email} \n Password: {site_password}')
             
+            
+            
           elif short_code == 'd':
             print(' ')
             if Credentials.find_vault(username, password):
               print('\n')
               for credentials in display_vault():
-                print(f' Site name: {credentials.site}')
+                print(f' Credentials for {credentials.site.capitalize()}')
                 print(f' Site username: {credentials.site_username}')
                 print(f' Site email: {credentials.site_email}')
                 print(f' Site password: {credentials.site_password}')
@@ -147,8 +137,35 @@ def main():
                 
             else:
               print(f'\nNo credentials found. Please try creating new credentials first')
-                
+              
           elif short_code == 'e':
+            while True:
+              print('\n')
+              print('Enter "m" to copy your your credentials or "n" to exit')
+              short_code = input("Enter option: ").lower()
+                
+              if short_code == 'm':
+                print('\nFilter credentials by site name\n')
+                copy_credentials = input("Enter site name: ").strip()
+                
+                for credentials in display_vault():
+                  if credentials.site == copy_credentials:
+                    pyperclip.copy(f'{credentials.site_username} {credentials.site_password}')
+                    print(f'\nCopied {credentials.site} to clipboard')
+                    break
+                    
+                  else:
+                    print('No credentials found')
+                    break
+                
+              elif short_code == 'n':
+                print('Exited successfully!')
+                break
+              else:
+                print('\nNo option specified')
+                break
+              
+          elif short_code == 'f':
             print('Are you sure you want to proceed? \nEnter "y" to logout \nEnter "n" to cancel')
             short_code = input("Enter option: ").lower()
             
@@ -161,10 +178,10 @@ def main():
               print('\nNo option specified')
               
           else:
-            print('\nNo option specified please try again')
+            print('\nNo option specified please try again and select from the list')
            
       else:    
-        print("\n You do not have an account with us please create one \n") 
+        print("\n You do not have an account with us please create one to continue \n") 
       
     elif short_code == 'x':
       print('Thank you for choosing Ficha Password Manager! See you later!')
